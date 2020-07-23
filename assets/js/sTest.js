8edb83a774c5a930c9f128c1602e69998256d153
Axel Gutiérrez Guevara <axel.g.guevara@gmail.com>
2020-07-23 17:33:49 -0500

    Style: Se integra jquery y cambian valores de scrollMagic.

--- assets/js/sTest.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-      $(".co").addClass("testco");
-    } else {
-      $(".co").removeClass("testco");
-    }
-  });
-});
