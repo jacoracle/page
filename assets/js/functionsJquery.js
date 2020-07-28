2eeb9f7e783b7f0007fec70a3baf51de2c6e7dd1
Axel Gutiérrez Guevara <axel.g.guevara@gmail.com>
2020-07-28 09:31:43 -0500

    Style: Se agrega back to top y se organizan funciones js.

--- assets/js/functionsJquery.js
+++ assets/js/functionsJquery.js
@@ -30,3 +30,26 @@
       }, 500);
 
   });
+
+// Back to top
+  if ($('#back-to-top').length) {
+      var scrollTrigger = 2000, // px
+          backToTop = function () {
+              var scrollTop = $(window).scrollTop();
+              if (scrollTop > scrollTrigger) {
+                  $('#back-to-top').addClass('show');
+              } else {
+                  $('#back-to-top').removeClass('show');
+              }
+          };
+      backToTop();
+      $(window).on('scroll', function () {
+          backToTop();
+      });
+      $('#back-to-top').on('click', function (e) {
+          e.preventDefault();
+          $('html,body').stop().animate({
+              scrollTop: 0
+          }, 700);
+      });
+  }
