d557fc737136f1a97973218e4f1ec133ae4a6a0e
Axel Gutiérrez Guevara <axel.g.guevara@gmail.com>
2020-07-28 16:43:12 -0500

    Se agregan vínculos  y se suben los archivos de páginas internas.

--- assets/js/functionsJquery.js
+++ assets/js/functionsJquery.js
@@ -53,3 +53,17 @@
           }, 700);
       });
   }
+
+// Internal page
+// Add class scroll element
+  $(window).scroll(function() {
+      var scrolle = $(window).scrollTop();
+      if (scrolle >= 550) {
+          $(".btn-back").addClass("btn-backScroll");
+          $(".miniheader-scroll").addClass("miniheader-scrollActive");
+      }
+      else {
+        $(".btn-back").removeClass("btn-backScroll");
+        $(".miniheader-scroll").removeClass("miniheader-scrollActive");
+      }
+  });
