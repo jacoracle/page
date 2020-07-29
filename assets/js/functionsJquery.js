8c3f87be5639499e7bb6fe6d9761fe2f79355e6c
Axel Gutiérrez Guevara <axel.g.guevara@gmail.com>
2020-07-29 12:04:07 -0500

    Style: Se agregan bloques de contenido grid en internal pages.

--- assets/js/functionsJquery.js
+++ assets/js/functionsJquery.js
@@ -53,17 +53,3 @@
           }, 700);
       });
   }
-
-// Internal page
-// Add class scroll element
-  $(window).scroll(function() {
-      var scrolle = $(window).scrollTop();
-      if (scrolle >= 550) {
-          $(".btn-back").addClass("btn-backScroll");
-          $(".miniheader-scroll").addClass("miniheader-scrollActive");
-      }
-      else {
-        $(".btn-back").removeClass("btn-backScroll");
-        $(".miniheader-scroll").removeClass("miniheader-scrollActive");
-      }
-  });
