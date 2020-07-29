25c3ef6aee9ec374a03459792807947880a3a8a1
Axel Gutiérrez Guevara <axel.g.guevara@gmail.com>
2020-07-29 16:11:27 -0500

    Se agregar función scrollMagic.

--- assets/js/runScrollM.js
+++ assets/js/runScrollM.js
@@ -1,11 +1,23 @@
-	const controller = new ScrollMagic.Controller();
-	const revealElements = document.getElementsByClassName("scroll-fade-in-up");
-	for (let i=0; i<revealElements.length; i++) {
-	  new ScrollMagic.Scene({
-	    triggerElement: revealElements[i],
-	    offset: 20,
-	    triggerHook: 0.9,
-	  })
-	    .setClassToggle(revealElements[i], "visible")
-	    .addTo(controller);
-	}
+
+const controller = new ScrollMagic.Controller();
+const revealElements = document.getElementsByClassName("scroll-fade-in-up");
+for (let i=0; i<revealElements.length; i++) {
+  new ScrollMagic.Scene({
+    triggerElement: revealElements[i],
+    offset: 20,
+    triggerHook: 0.9,
+  })
+    .setClassToggle(revealElements[i], "visible")
+    .addTo(controller);
+}
+
+// Agregar escenas para indicar punto de acción
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger1"})
+  .setClassToggle("#plane", "movePlane")
+  .addIndicators({name: "1 - add a class"})
+  .addTo(controller);
+
+var scene2 = new ScrollMagic.Scene({triggerElement: "#trigger2"})
+  .setClassToggle("#plane", "movePlane")
+  .addIndicators({name: "2 - remove a class"})
+  .addTo(controller);
